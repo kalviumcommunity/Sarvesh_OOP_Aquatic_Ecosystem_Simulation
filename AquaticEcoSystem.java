b9857c71b6e619b9333434313d5420dc412df5a4
Sarvesh <sarveshjaiofficial@gmail.com>
2024-08-31 14:56:34 +0530

    Added more objects

--- AquaticEcoSystem.java
+++ AquaticEcoSystem.java
@@ -3,9 +3,13 @@ public class AquaticEcoSystem {
 
         Fish shark = new Fish("Shark", 300.0, "Smaller Fish", "Coral Reef");
         Fish clownfish = new Fish("Clownfish", 5.0, "Algae and Plankton", "Sea Anemone");
+        Fish tuna = new Fish("Tuna", 3.5, "Small Fish", "Deep Sea");
+
 
         Plant seaweed = new Plant("Seaweed", "Coral Reef");
         Plant algae = new Plant("Algae", "Shallow Waters");
+        Plant kelp = new Plant("Kelp", "Shallow Waters");
+        Plant seagrass = new Plant("Seagrass", "Coastal Areas");
 
         shark.move();
         shark.feed();
